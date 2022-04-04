9d256a7f2053cfea14bd80744b688600b76ea5da
vghybrid <anderson.sarmiento13@gmail.com>
2022-04-04 10:08:21 -0300

    Cambios Migraciones

--- database/migrations/2022_04_04_124639_create_posts_table.php
+++ database/migrations/2022_04_04_124639_create_posts_table.php
@@ -11,13 +11,13 @@ return new class extends Migration {
      * @return void
      */
     public function up() {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->string('title', 255);
+            $table->string('slug', 255);
+            $table->text('content');
+            $table->string('image');
+            $table->enum('posted', ['yes', 'not']);
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration {
      * @return void
      */
     public function down() {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('posts');
     }
 };
---
+++ resources/views/fragment/subview.blade.php
@@ -0,0 +1,2 @@
+    <h1>Sub vista 1</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio id, autem voluptatibus laborum aliquam ipsam dignissimos dolores illo facere rem alias veniam voluptate laboriosam nulla ducimus vero eaque quasi. Porro?</p>
